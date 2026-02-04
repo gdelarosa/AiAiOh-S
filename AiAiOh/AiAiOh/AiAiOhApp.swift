b92371eacbb1448e3f01dd63414fc07c7ba6a875
Gina <ginacdlr@gmail.com>
2026-02-04 13:40:33 -0800

    New Xcode Project
    Create new app project

---
+++ AiAiOh/AiAiOh/AiAiOhApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiAiOhApp.swift
+//  AiAiOh
+//
+//  Created by Gina on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct AiAiOhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
